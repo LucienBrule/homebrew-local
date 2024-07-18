3af0c4512ffb6df9e4982283996e54ff685eace3
Lucien Brule <lucien@margin.re>
2024-07-18 14:46:41 -0400

    Update SHA256 hash for nfsmount

--- Formula/nfsmount.rb
+++ Formula/nfsmount.rb
@@ -2,7 +2,7 @@ class Nfsmount < Formula
   desc "NFS mount service for specific share"
   homepage "https://github.com/LucienBrule/homebrew-local"
   url "https://github.com/LucienBrule/homebrew-local/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "6bab4f893d712e3bf257b0b52b8be2d568c5ec6e0c8869e9c177e6223cec9d5d"
   license "MIT"
 
   def install
