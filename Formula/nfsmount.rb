bbab76e85d27918e4e69442b56cacc13395ab242
Lucien Brule <lucien@margin.re>
2024-07-18 14:37:57 -0400

    add shasum

--- Formula/nfsmount.rb
+++ Formula/nfsmount.rb
@@ -2,7 +2,7 @@ class Nfsmount < Formula
   desc "NFS mount service for specific share"
   homepage "https://github.com/LucienBrule/homebrew-local"
   url "https://github.com/LucienBrule/homebrew-local/archive/v1.0.0.tar.gz"
-  sha256 "the_actual_sha256_of_your_tarball"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
 
   def install
